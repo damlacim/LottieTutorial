115d9b355607c97fb3a880c5f9b3dbb6cfe05b19
damlacim <cim.damla@gmail.com>
2022-05-23 15:57:51 +0300

    set up animation

--- LottieTutorial/ViewController.swift
+++ LottieTutorial/ViewController.swift
@@ -6,14 +6,34 @@
 //
 
 import UIKit
+import Lottie
 
 class ViewController: UIViewController {
-
+    
+    var animationView: AnimationView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupAnimation()
+        playAnimation()
     }
-
-
+    
+    func setupAnimation() {
+        //Start animation
+        animationView = .init(name: "developer")
+        animationView!.frame = view.bounds
+        //Set animation content mode
+        animationView!.contentMode = .scaleAspectFit
+        //Set animation loop mode
+        animationView!.loopMode = .loop
+        //Animation speed
+        animationView!.animationSpeed = 1.5
+        view.addSubview(animationView!)
+    }
+    
+    func playAnimation() {
+        //Play
+        animationView?.play()
+    }
 }
 
